2196119b8f6412dba3ef971fad6a36b57f3173f8
gonzalopena <gonzalopena78.gp@gmail.com>
2025-10-02 17:06:25 -0300

    suma y resta

---
+++ python ejercicios/secuencial/ejercicio2.py
@@ -0,0 +1,25 @@
+# segundo ejercio
+
+"""
+ingresar nombre y 2 valores
+"""
+
+nombre = None
+num1 = None
+num2 = None
+
+print ("ingresa tu nombre y dos valores enteros:")
+
+nombre = input("ingresa tu nombre:")
+
+num1 = int(input("ingresa el primer valor:"))
+
+num2 = int(input("ingresa el segundo valor:"))
+
+resultado = num1 - num2
+
+print(nombre, "tu resultado es:", resultado)
+
+resultado = num1 + num2
+
+print(nombre, "tu suma es:", resultado)
