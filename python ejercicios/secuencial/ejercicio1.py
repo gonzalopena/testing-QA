f6f033332013554ac9ba37eaa29400daba9079a5
gonzalopena <gonzalopena78.gp@gmail.com>
2025-10-02 17:07:32 -0300

    suma

--- python ejercicios/secuencial/ejercicio1.py
+++ python ejercicios/secuencial/ejercicio1.py
@@ -1,1 +1,22 @@
-print("Hello")
+# Suma de dos numeros
+"""
+Comentario en python
+"""
+
+# ingreso variables
+nombre = None
+num_1 = None
+num_2 = None
+
+
+print("Pedir al usuario que ingrese su nombre, dos numeros y se sumen:")
+
+nombre = input("Decime tu nombre:")
+
+num_1 = int(input("ingresa el primer valor:"))
+
+num_2 = int(input("ingresa el segundo valor:")) 
+
+resultado = num_1 + num_2
+
+print (nombre,"tu resultado es:", resultado)
